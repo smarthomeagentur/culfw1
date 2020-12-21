180dcb5031b845a4d29054c35293408b1d1d8a38
danielboe <danielboeber@gmail.com>
2020-12-21 16:45:27 +0100

    fix(RF)small freq change feat(build)mbus target

--- culfw/Devices/CUL/board.h
+++ culfw/Devices/CUL/board.h
@@ -27,24 +27,28 @@
 #define CUL_V3
 #endif
 
+#if defined(CUL_V3_MBUS)
+#define CUL_V3
+#endif
+
 #if defined(CUL_V3) || defined(CUL_V4)
-#define HAS_FHT_8v // PROGMEM:  586b  RAM: 23b
-#define HAS_FHT_TF
-#define FHTBUF_SIZE 174 //                 RAM: 174b
-#define RCV_BUCKETS 4	//                 RAM: 25b * bucket
-#define FULL_CC1100_PA	// PROGMEM:  108b
-#define HAS_RAWSEND		//
-#define HAS_ASKSIN		// PROGMEM: 1314
-#define HAS_ASKSIN_FUP	// PROGMEM:   78
-#define HAS_MORITZ		// PROGMEM: 1696
-#define HAS_ESA			// PROGMEM:  286
-#define HAS_TX3			// PROGMEM:  168
-#define HAS_INTERTECHNO // PROGMEM: 1352
-#define HAS_TCM97001	// PROGMEM:  264
-#define HAS_UNIROLL		// PROGMEM:   92
-#define HAS_MEMFN		// PROGMEM:  168
-//#  define HAS_SOMFY_RTS                 // PROGMEM: 1716
-#define HAS_BELFOX // PROGMEM:  214
+#  define HAS_FHT_8v                    // PROGMEM:  586b  RAM: 23b
+#  define HAS_FHT_TF
+#  define FHTBUF_SIZE          174      //                 RAM: 174b
+#  define RCV_BUCKETS            4      //                 RAM: 25b * bucket
+#  define FULL_CC1100_PA                // PROGMEM:  108b
+#  define HAS_RAWSEND                   //
+#  define HAS_ASKSIN                    // PROGMEM: 1314
+#  define HAS_ASKSIN_FUP                // PROGMEM:   78
+#  define HAS_MORITZ                    // PROGMEM: 1696
+#  define HAS_ESA                       // PROGMEM:  286
+#  define HAS_TX3                       // PROGMEM:  168
+#  define HAS_INTERTECHNO               // PROGMEM: 1352
+#  define HAS_TCM97001                  // PROGMEM:  264
+#  define HAS_UNIROLL                   // PROGMEM:   92
+#  define HAS_MEMFN                     // PROGMEM:  168
+#  define HAS_SOMFY_RTS                 // PROGMEM: 1716
+#  define HAS_BELFOX                    // PROGMEM:  214
 #endif
 
 #if defined(CUL_V4)
@@ -54,12 +58,12 @@
 
 #if defined(CUL_V3)
 #define TTY_BUFSIZE 128 // RAM: TTY_BUFSIZE*4
-#define HAS_MBUS		// PROGMEM: 4255
+#  undef HAS_MBUS                       // PROGMEM: 4255
 #if defined(HAS_MBUS)
 #define MBUS_NO_TX // PROGMEM:  962
 #endif
 #define HAS_RFNATIVE // PROGMEM:  580
-//#  define HAS_KOPP_FC                   // PROGMEM: 3370
+#  define HAS_KOPP_FC                   // PROGMEM: 3370
 #endif
 
 #if defined(CUL_V3_ZWAVE)
@@ -68,6 +72,18 @@
 #undef HAS_KOPP_FC
 #undef HAS_RFNATIVE
 #define LACROSSE_HMS_EMU // PROGMEM: 2206
+#define HAS_EVOHOME
+#endif
+
+#if defined(CUL_V3_MBUS)
+#undef HAS_ZWAVE // PROGMEM:  882
+#define HAS_MBUS
+#if defined(HAS_MBUS)
+#define MBUS_NO_TX // PROGMEM:  962
+#endif
+#undef HAS_KOPP_FC
+#undef HAS_RFNATIVE
+#undef LACROSSE_HMS_EMU // PROGMEM: 2206
 #define HAS_EVOHOME
 #endif
 
